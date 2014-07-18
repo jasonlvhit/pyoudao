9980ba52ca956d3218fb76cb419120f0ce22b64b
jasonlvhit <jasonlvhit@gmail.com>
2014-07-18 13:20:15 +0800

    add gitignore

--- pyoudao/pyoudao.py
+++ pyoudao/pyoudao.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-  
+# -*- coding: utf-8 -*-
 import argparse
 import json
 import requests
@@ -11,75 +11,81 @@ DOCTYPE = "json"
 KEYFROM = "pyoudao"
 KEY = "108579965"
 
-class pyoudaoException(Exception):
-	
-	errorCode = { 0: "正常",
-				  20: "要翻译的文本过长",
-				  30: "无法进行有效的翻译",
-				  40: "不支持的语言类型",
-				  50: "无效的key",
-				  60: "无词典结果，仅在获取词典结果生效"}
 
-	def __init__(self, code):
-		assert code in (self.errorCode.keys())
-		self.value = self.errorCode[code] 
-	def __repr__(self):
-		return 'pyoudao execption : %s', self.value
+class pyoudaoException(Exception):
+
+    errorCode = {0: "正常",
+                 20: "要翻译的文本过长",
+                 30: "无法进行有效的翻译",
+                 40: "不支持的语言类型",
+                 50: "无效的key",
+                 60: "无词典结果，仅在获取词典结果生效"}
+
+    def __init__(self, code):
+        assert code in (self.errorCode.keys())
+        self.value = self.errorCode[code]
+
+    def __repr__(self):
+        return 'pyoudao execption : %s', self.value
+
 
 def request(q):
-	params = {"version": Y_VERSION,
-			  "type": TYPE,
+    params = {"version": Y_VERSION,
+              "type": TYPE,
               "doctype": DOCTYPE,
-			  "keyfrom": KEYFROM,
-              "key":KEY,
-			  "q": q }
-	try:
-		res = requests.get(BASE_URL, params = params).json()
-	except requests.exceptions.RequestsException as e:
-		print(e.message)
+              "keyfrom": KEYFROM,
+              "key": KEY,
+              "q": q}
+    try:
+        res = requests.get(BASE_URL, params=params).json()
+    except requests.exceptions.RequestsException as e:
+        print(e.message)
 
-	return res 
+    return res
+
 
 def show(res):
-	if res['errorCode'] != 0:
-		raise pyoudaoException(res['errorCode'])
-	
-	print("==========pyoudao===========")
-	print("查询:"),
-	print(res['query'])
-	print("翻译:"), 
-	print(','.join(res['translation']))
-	print("-------------------")
-	
-	if 'basic' not in res:
-		return 
+    if res['errorCode'] != 0:
+        raise pyoudaoException(res['errorCode'])
 
-	print("基本释义:")
-	for i in res['basic']['explains']:
-		print(i)
-	
-	print("-------------------")
-	if 'phonetic' in res['basic']:
-		print("标准发音:"),
-		print(res['basic']['phonetic'])
-		print("美式发音:"),
-		print(res['basic']['us-phonetic'])
-		print("英式发音:"),
-		print(res['basic']['uk-phonetic'])
+    print("==========pyoudao===========")
+    print("查询:"),
+    print(res['query'])
+    print("翻译:"),
+    print(','.join(res['translation']))
+    print("-------------------")
 
-	if not res['web']:
-		return
-	print("\n网络释义\n----------------------:")
-	for i in res['web']:
-		print("关键词:"),
-		print(i['key'])	
-		print("释义:"),
-		print(','.join(i['value']))
-	print("\n\n")	
+    if 'basic' not in res:
+        return
+
+    print("基本释义:")
+    for i in res['basic']['explains']:
+        print(i)
+
+    print("-------------------")
+    if 'phonetic' in res['basic']:
+        print("标准发音:"),
+        print(res['basic']['phonetic'])
+        print("美式发音:"),
+        print(res['basic']['us-phonetic'])
+        print("英式发音:"),
+        print(res['basic']['uk-phonetic'])
+
+    if not res['web']:
+        return
+    print("\n网络释义\n----------------------:")
+    for i in res['web']:
+        print("关键词:"),
+        print(i['key'])
+        print("释义:"),
+        print(','.join(i['value']))
+    print("\n\n")
+
 
 def pretty_print(f):
-	print(json.dumps(f, sort_keys = True,
-			indent = 4, separators = (',', ':')))
+    print(json.dumps(f, sort_keys=True,
+                     indent=4, separators=(',', ':')))
+
 
 def get_parser():
     parser = argparse.ArgumentParser(
@@ -88,13 +94,12 @@ def get_parser():
                         help='translate the keywords or text')
     return parser
 
+
 def command_line_runner():
-	parser = get_parser()
-	args = vars(parser.parse_args())
-	show(request(' '.join(args['fanyi'])))
-	return
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    show(request(' '.join(args['fanyi'])))
+    return
 
 if __name__ == "__main__":
-	command_line_runner()
-	
-	
+    command_line_runner()
